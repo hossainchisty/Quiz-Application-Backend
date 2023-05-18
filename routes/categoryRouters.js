8bcc099c7d02f44b1bb2e5560f679fb25ad584e0
hossainchisty <hossain.chisty11@gmail.com>
2023-05-18 17:55:17 +0600

    feat: Add category Routers
    This code adds category routers to handle CRUD operations for categories. It includes functions to get, add, update and delete categories. Additionally, it includes middleware for authentication.

---
+++ routes/categoryRouters.js
@@ -0,0 +1,22 @@
+// Basic Lib Imports
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  getCategory,
+  addCategory,
+  updateCategory,
+  deleteCategory,
+} = require('../controllers/categoryController');
+
+const { protect } = require('../middleware/authMiddleware');
+
+// Adding and getting categories
+router.route('/').get(getCategory).post(addCategory);
+
+// Deleting and updating categories
+router.route('/:id').delete(protect, deleteCategory).put(protect, updateCategory);
+
+// Exporting the Router
+module.exports = router;
