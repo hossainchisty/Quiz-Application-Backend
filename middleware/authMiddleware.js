331f7cb465aa593b0eb4bbb02110b8f5a3573847
hossainchisty <hossain.chisty11@gmail.com>
2023-05-18 17:10:19 +0600

    feat: Added middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,41 @@
+// Basic Lib Imports
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModels');
+
+/**
+ * @desc Middleware that verify user authorization
+ * @protected True
+ * @author Hossain Chisty
+ * @abstract Middle
+ */
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      // eslint-disable-next-line prefer-destructuring
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Please authenticate');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+module.exports = { protect };
