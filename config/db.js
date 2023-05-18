26cd534faa58d7f37bb154aa9180924d7e911fdd
hossainchisty <hossain.chisty11@gmail.com>
2023-05-18 18:20:24 +0600

    refactor: Remove console log for MongoDB connection status
    The console log for displaying the MongoDB connection status was removed in order to avoid clutter in the console output.

--- config/db.js
+++ config/db.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 // Mongo DB Database Connection
 const connectDB = async () => {
   try {
+    // eslint-disable-next-line no-unused-vars
     const connect = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`MongoDB Connected: ${connect.connection.host}`);
+    // console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
