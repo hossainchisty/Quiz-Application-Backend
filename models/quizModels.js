022e784dadcb4a33cf6aec3b8b4e88bd5146d749
hossainchisty <hossain.chisty11@gmail.com>
2023-05-19 12:01:27 +0600

    refactor: quizModels.js to add instance methods for calculating the user's score and progress. Also, make `questionId` and `User` fields optional.

--- models/quizModels.js
+++ models/quizModels.js
@@ -1,3 +1,8 @@
+/* eslint-disable func-names */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-shadow */
 // Basic Lib Imports
 const mongoose = require('mongoose');
 
@@ -18,7 +23,7 @@ const quizSchema = new mongoose.Schema(
           required: true,
         },
         options: [String],
-        answers: [{ type: String }],
+        correctAnswer: Number,
       },
     ],
     userAnswers: [
@@ -26,16 +31,16 @@ const quizSchema = new mongoose.Schema(
         questionId: {
           type: mongoose.Schema.Types.ObjectId,
           ref: 'Question',
-          required: true,
+          required: false,
         },
         selectedAnswer: Number,
         isCorrect: Boolean,
       },
     ],
-    User: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true,
+      required: false,
     },
     categories: [
       {
@@ -50,5 +55,28 @@ const quizSchema = new mongoose.Schema(
     versionKey: false,
   }
 );
+
+// Instance method to calculate the user's score and update the userAnswers array
+quizSchema.methods.calculateScore = function () {
+  let score = 0;
+  this.userAnswers.forEach((answer) => {
+    const question = this.questions.find((question) => question._id.equals(answer.questionId));
+    if (question && question.correctAnswer === answer.selectedAnswer) {
+      answer.isCorrect = true;
+      score++;
+    } else {
+      answer.isCorrect = false;
+    }
+  });
+  return score;
+};
+
+// Instance method to get the user's progress through the quiz
+quizSchema.methods.getProgress = function () {
+  const totalQuestions = this.questions.length;
+  const answeredQuestions = this.userAnswers.filter((answer) => answer.isCorrect).length;
+  const progress = (answeredQuestions / totalQuestions) * 100;
+  return progress;
+};
 
 module.exports = mongoose.model('Quiz', quizSchema);
